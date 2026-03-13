594ced506c0756dd1ab10e7029ab7931d39368ac
Grenixx <enzomyo@gmail.com>
2026-03-13 19:35:15 +0100

    smooth bloc

--- tqdm.c
+++ tqdm.c
@@ -1,103 +1,67 @@
 #include <stdio.h>
 #include <time.h>
-#include <unistd.h>
 
 #ifdef _WIN32
 #include <windows.h>
 #endif
 
+// Niveaux de remplissage
+const char *blocks[] = {" ", "▏", "▎", "▍", "▌", "▋", "▊", "▉", "█"};
+#define BLOCKS 8
+
 void progress_bar(long long current, long long total)
 {
-    // depart de la clock a 0 pour init toujour demarer progressbar a 0
     static time_t start;
     if (current == 0)
     {
 #ifdef _WIN32
         SetConsoleOutputCP(CP_UTF8);
 #endif
+
         start = time(NULL);
     }
-    time_t currtime = time(NULL);
-    unsigned long secondPastsinceStart = (unsigned long)difftime(currtime, start);
 
-    static int last_percent = -1;
-    int percent = (int)(((current + 1) * 100) / total);
-    if (current >= total)
-        percent = 100;
-    if (percent == last_percent)
-        return;
-    last_percent = percent;
+    time_t now = time(NULL);
+    unsigned long elapsed = (unsigned long)difftime(now, start);
 
-    int width = 30;
-    int filled = (int)(((current + 1) * width) / total);
-
-    printf("\r|");
-    for (int i = 0; i < width; i++)
-        if (i < filled)
-            printf("█");
-        else if (i == filled)
-            printf("▓");
-        else
-        {
-            printf("▒");
-        }
-
-    printf("| %3d%% %lus", percent, secondPastsinceStart);
-    if (percent == 100)
-        printf("\n");
-    fflush(stdout);
-}
-
-/*
-█
-▓
-▒
-░
-*/
-// #include <windows.h>
-int main()
-{
-    // SetConsoleOutputCP(CP_UTF8);
-    for (int i = 0; i < 100000000; i++)
-    {
-        progress_bar(i, 100000000);
-    }
-}
-
-/*void progress(int current, int total)
-{
-    int percent = (current * 100) / total;
-    printf("\rProgress: %3d%%", percent);
-    //fflush(stdout);
-}*/
-/*
-void progress_bar(int current, int total)
-{
-    int width = 30;
-    int filled = (current * width) / total;
-
-    printf("\r[");
-    for (int i = 0; i < width; i++)
-        printf(i < filled ? "O" : "-");
-    printf("] %3d%%", (current * 100) / total);
-    fflush(stdout);
-}
-*/
-
-/*void progress_bar(long long current, long long total)
-{
     static int last_percent = -1;
     int percent = (int)((current * 100) / total);
     if (percent == last_percent)
         return;
     last_percent = percent;
 
-    int width = 30;
-    int filled = (int)((current * width) / total);
+    int width = 30; // largeur de la barre
+    double ratio = (double)current / total;
+    double filled_blocks = ratio * width * BLOCKS;
+    int full = (int)(filled_blocks / BLOCKS);
+    int partial = (int)(filled_blocks) % BLOCKS;
 
     printf("\r[");
     for (int i = 0; i < width; i++)
-        printf(i < filled ? "O" : "-");
-    printf("] %3d%%", percent);
+    {
+        if (i < full)
+            printf("%s", blocks[BLOCKS]);
+        else if (i == full)
+            printf("%s", blocks[partial]);
+        else
+            printf(" ");
+    }
+    printf("] %3d%% %lus", percent, elapsed);
     fflush(stdout);
-}*/
+
+    if (percent == 100)
+        printf("\n");
+}
+
+int main()
+{
+
+    long long total = 100000000;
+
+    for (long long i = 0; i <= total; i++)
+    {
+        progress_bar(i, total);
+    }
+
+    return 0;
+}
